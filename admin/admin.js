f940a6afc0fc97fb2ba53b7f1c90fef9524f0ea1
TakahashiYoji <yoji.takahashi@relic.co.jp>
2023-12-30 11:48:31 +0900

    2024年度授業用

---
+++ admin/admin.js
@@ -0,0 +1,34 @@
+// アクセス数を取得
+const getDateData = Object.keys(localStorage);
+const getAccessData = Object.values(localStorage);
+
+let lineCtx = document.getElementById("lineChart");
+let lineConfig = {
+  type: 'bar',
+  data: {    
+    labels: getDateData,
+    datasets: [{
+      label: 'アクセス数',
+      data: getAccessData,
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    }],
+  },
+  options: {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: '日付',
+        }
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'アクセス数',
+        }
+      }
+    },
+  },
+};
+let lineChart = new Chart(lineCtx, lineConfig);
